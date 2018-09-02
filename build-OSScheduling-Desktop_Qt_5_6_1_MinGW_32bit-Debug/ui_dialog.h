c19605abb7314263c6f620779d31a04f12c1b3ea
Ti-tanium <1158976264@qq.com>
2018-09-02 10:12:14 +0800

    Initial commit

---
+++ build-OSScheduling-Desktop_Qt_5_6_1_MinGW_32bit-Debug/ui_dialog.h
@@ -0,0 +1,95 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOG_H
+#define UI_DIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTextEdit>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog
+{
+public:
+    QGridLayout *gridLayout;
+    QTextEdit *textEdit;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
+    QPushButton *ExitBtn;
+    QDialogButtonBox *buttonBox;
+
+    void setupUi(QDialog *Dialog)
+    {
+        if (Dialog->objectName().isEmpty())
+            Dialog->setObjectName(QStringLiteral("Dialog"));
+        Dialog->resize(400, 300);
+        gridLayout = new QGridLayout(Dialog);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        textEdit = new QTextEdit(Dialog);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+
+        gridLayout->addWidget(textEdit, 0, 0, 1, 1);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        pushButton = new QPushButton(Dialog);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout->addWidget(pushButton);
+
+        ExitBtn = new QPushButton(Dialog);
+        ExitBtn->setObjectName(QStringLiteral("ExitBtn"));
+
+        horizontalLayout->addWidget(ExitBtn);
+
+
+        gridLayout->addLayout(horizontalLayout, 1, 0, 1, 1);
+
+        buttonBox = new QDialogButtonBox(Dialog);
+        buttonBox->setObjectName(QStringLiteral("buttonBox"));
+        buttonBox->setOrientation(Qt::Horizontal);
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+
+        gridLayout->addWidget(buttonBox, 2, 0, 1, 1);
+
+
+        retranslateUi(Dialog);
+        QObject::connect(buttonBox, SIGNAL(accepted()), Dialog, SLOT(accept()));
+        QObject::connect(buttonBox, SIGNAL(rejected()), Dialog, SLOT(reject()));
+        QObject::connect(pushButton, SIGNAL(clicked()), Dialog, SLOT(accept()));
+        QObject::connect(ExitBtn, SIGNAL(clicked()), Dialog, SLOT(close()));
+
+        QMetaObject::connectSlotsByName(Dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog)
+    {
+        Dialog->setWindowTitle(QApplication::translate("Dialog", "Dialog", 0));
+        pushButton->setText(QApplication::translate("Dialog", "login", 0));
+        ExitBtn->setText(QApplication::translate("Dialog", "Exit", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog: public Ui_Dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOG_H
